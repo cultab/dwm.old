5c7098b5acc766adc0976bca9dcac6f090615c93
cultab <rroarck@gmail.com>
2020-05-07 16:00:42 +0300

    welp

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 8;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Terminus:style=Regular:pixelsize=15:antialias=true:autohint=true", "monospace:size=10" };
-static const char dmenufont[]       = "Terminus:style=Regular:pixelsize=15:antialias=true:autohint=true";
+static const char dmenufont[]       =   "Terminus:style=Regular:pixelsize=15:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -24,9 +24,9 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { " Web", " Dev", " File", " Disc", " bg" };
+static const char *tags[] = { " Web:1", " Dev:2", " Dev:3", " Dev:4", " Chat:5", " Sys:6" };
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-//static const char *tags[] = { "➊", "➋", "➌", "➍", "➎", "➏", "➐", "➑", "➒" };
+//static const char *tags[] = { "➊", "➋", "➌", "➍", "➎", "➏", "➐", "➑", "➒" };
 //static const char *tags[] = { "", "", "", "", "", "", "❽", "", "" };
 
 /* xprop(1):
@@ -37,9 +37,10 @@ static const Rule rules[] = {
 	/* class               instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",              NULL,       NULL,       0,            1,           -1 },
 	{ "Vivaldi-Stable",    NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "discord",           NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "MEGAsync",          NULL,       NULL,       1 << 4,       1,           -1 },
-	{ "Barrier",           NULL,       NULL,       1 << 4,       1,           -1 },
+	{ "discord",           NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "MEGAsync",          NULL,       NULL,       1 << 5,       1,           -1 },
+	{ "Barrier",           NULL,       NULL,       1 << 5,       1,           -1 },
+	{ "keepassxc",         NULL,       NULL,       1 << 5,       1,           -1 },
 };
 
 /* layout(s) */
@@ -48,10 +49,10 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	/* symbol arrange function */
+	{ "[T]",    tile },    /* first entry is default */
+	{ "[F]",    NULL },    /* no layout function means floating behavior */
+	{ "[M]",    monocle },
 };
 
 /* key definitions */
@@ -70,22 +71,24 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[]     = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_bgrey, NULL };
 static const char *tmux_termcmd[] = { "st", "-e", "tmux" };
 static const char *termcmd[]      = { "st", NULL };
-static const char *fffcmd[]      =  { "st", "-e", "fff" };
+//static const char *fffcmd[]      =  { "st", "-e", "fff" };
 static const char *mutecmd[]      = { "amixer", "-q", "set", "Master", "toggle", NULL };
-static const char *volupcmd[]     = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
-static const char *voldowncmd[]   = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
+static const char *volupcmd[]     = { "amixer", "-q", "set", "Master", "10%+", "unmute", NULL };
+static const char *voldowncmd[]   = { "amixer", "-q", "set", "Master", "10%-", "unmute", NULL };
 static const char *miccmd[]       = { "amixer", "set", "Capture", "toggle", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 static Key keys[] = {
 	/* modifier                     key                      function        argument */
 	{ MODKEY,                       XK_r,                    spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return,               spawn,          {.v = tmux_termcmd } },
 	{ MODKEY|ControlMask,           XK_Return,               spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_f,                    spawn,          {.v = fffcmd } },
     { 0,                            XF86XK_AudioMute,        spawn,          {.v = mutecmd } },
     { 0,                            XF86XK_AudioLowerVolume, spawn,          {.v = voldowncmd } },
     { 0,                            XF86XK_AudioRaiseVolume, spawn,          {.v = volupcmd } },
     { 0,                            XF86XK_AudioMicMute,     spawn,          {.v = miccmd } },
+	{ MODKEY,                       XK_grave,                togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,                    togglebar,      {0} },
 	{ MODKEY,                       XK_j,                    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,                    focusstack,     {.i = -1 } },
@@ -100,7 +103,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,                  view,           {0} },
 	{ MODKEY|ShiftMask,             XK_d,                    killclient,     {0} },
 	{ MODKEY,                       XK_t,                    setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_n,                    setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,                    setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,                    setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,                setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,                togglefloating, {0} },
